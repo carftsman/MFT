9fdc5c5520da6066983273f3c0c82636b6b3fdec
Naveendbs137 <venkatanaveen.t@dhatvibs.com>
2026-02-16 18:09:45 +0530

    first commit

---
+++ src/main/java/com/dhatvibs/MfoApplication.java
@@ -0,0 +1,13 @@
+package com.dhatvibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfoApplication.class, args);
+	}
+
+}
