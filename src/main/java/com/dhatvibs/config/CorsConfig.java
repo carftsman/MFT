10bb4a7fd714979c1ba1ea83cbe8dd8202906d4c
Naveendbs137 <venkatanaveen.t@dhatvibs.com>
2026-03-24 15:49:57 +0530

    cors problem

--- src/main/java/com/dhatvibs/config/CorsConfig.java
+++ src/main/java/com/dhatvibs/config/CorsConfig.java
@@ -52,7 +52,7 @@ public class CorsConfig {
                        // .allowedOrigins("http://localhost:3000")
                         //.allowedOrigins("http://localhost:3000","https://performance-dashboard-umber.vercel.app")
                         //.allowedOrigins("http://fieldconnect.dhatvibs.com","http://localhost:3000")
-                		.allowedOrigins("https://performance-dashboard-fe.onrender.com")
+                		.allowedOrigins("https://performance-dashboard-fe.onrender.com","http://localhost:3000")
                         .allowCredentials(true)
                         .allowedMethods("*");
             }
