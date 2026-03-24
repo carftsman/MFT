29b2146ca2df558fe5834def1a941b988af8f901
Naveendbs137 <venkatanaveen.t@dhatvibs.com>
2026-03-24 16:11:46 +0530

    allow all type of origins

--- src/main/java/com/dhatvibs/config/CorsConfig.java
+++ src/main/java/com/dhatvibs/config/CorsConfig.java
@@ -52,7 +52,8 @@ public class CorsConfig {
                        // .allowedOrigins("http://localhost:3000")
                         //.allowedOrigins("http://localhost:3000","https://performance-dashboard-umber.vercel.app")
                         //.allowedOrigins("http://fieldconnect.dhatvibs.com","http://localhost:3000")
-                		.allowedOrigins("https://performance-dashboard-fe.onrender.com","http://localhost:3000")
+                		//.allowedOrigins("https://performance-dashboard-fe.onrender.com","http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowCredentials(true)
                         .allowedMethods("*");
             }
